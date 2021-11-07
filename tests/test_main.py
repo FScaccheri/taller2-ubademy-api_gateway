6bb31631df3735fe08509cee077bbcead120d013
Ivan <isoriano@fi.uba.ar>
2021-11-07 12:27:02 -0300

    Fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,4 @@ def test_home():
     response = client.get('/')
     assert response.status_code != 400
     assert response.status_code == 200
-    assert response.json() == {'message': 'Hello API gateway!'}
+    assert response.json() == {'status':'ok','message': 'Hello API gateway!'}
