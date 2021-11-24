718387c0f0b7df07e2f3265701aa2f5001ac1919
Franco Scaccheri <fcassanello@gitlab.com>
2021-11-23 22:27:51 -0300

    Extract tests into folders. Add tests for business endpoints

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,14 @@
-from unittest.mock import patch, MagicMock
 from fastapi.testclient import TestClient
-from main import app
+from main import app, authenticate_token
+from models.tokens import TokenData
+
+
+def authenticate_token_override():
+    return TokenData(email='mail@mail.com', is_admin=False)
+
+
+app.dependency_overrides[authenticate_token] = authenticate_token_override
+
 
 client = TestClient(app)
 
@@ -10,82 +18,3 @@ def test_home():
     assert response.status_code != 400
     assert response.status_code == 200
     assert response.json() == {'status': 'ok', 'message': 'Hello API gateway!'}
-
-
-@patch('main.requests.post')
-def test_successful_login(mock_users_login):
-    mock_users_login.return_value = MagicMock(status_code=200)
-    mock_users_login.return_value.json.return_value = {'status': 'ok'}
-
-    response = client.post('/login', json={
-        'email': 'mail@mail.com',
-        'password': 'secret_password'
-    })
-    response_data = response.json()
-
-    assert response.status_code != 400
-    assert response.status_code == 200
-    assert response_data['status'] == 'ok'
-    assert 'access_token' in response_data
-    assert response_data['token_type'] == 'bearer'
-
-
-@patch('main.requests.post')
-def test_failed_login(mock_users_login):
-    mock_users_login.return_value = MagicMock(status_code=200)
-    mock_users_login.return_value.json.return_value = {'status': 'error'}
-
-    response = client.post('/login', json={
-        'email': 'non_existent_mail@mail.com',
-        'password': 'non_existent_password'
-    })
-    response_data = response.json()
-
-    assert response.status_code != 400
-    assert response.status_code == 200
-    assert response_data['status'] == 'error'
-    assert response_data['message'] == 'error_bad_login'
-
-
-@patch('main.requests.post')
-def test_successful_sign_up(mock_users_sign_up):
-    email = 'mail@mail.com'
-    name = 'John Doe'
-
-    mock_users_sign_up.return_value = MagicMock(status_code=200)
-    mock_users_sign_up.return_value.json.return_value = {
-        'status': 'ok',
-        'email': email,
-        'name': name
-    }
-
-    response = client.post('/sign_up', json={
-        'email': email,
-        'password': 'secret_password',
-        'name': name
-    })
-    response_data = response.json()
-
-    assert response.status_code != 400
-    assert response.status_code == 200
-    assert response_data['status'] == 'ok'
-
-
-@patch('main.requests.post')
-def test_failed_sign_up(mock_users_sign_up):
-    email = 'mail@mail.com'
-    name = 'John Doe'
-
-    mock_users_sign_up.return_value = MagicMock(status_code=200)
-    mock_users_sign_up.return_value.json.return_value = {'status': 'error'}
-
-    response = client.post('/sign_up', json={
-        'email': email,
-        'password': 'secret_password',
-        'name': name
-    })
-    response_data = response.json()
-
-    assert response.status_code != 400
-    assert response.status_code == 200
-    assert response_data['status'] == 'error'
