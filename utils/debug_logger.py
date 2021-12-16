c052fa40dda56282e3dcbdcf722dcdc9510bfd51
Franco Scaccheri <fcassanello@gitlab.com>
2021-12-16 18:59:07 -0300

    Add FileHandler for local debug logger

--- utils/debug_logger.py
+++ utils/debug_logger.py
@@ -8,7 +8,14 @@ def debug_logger():
 
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler('logs.log')
+    file_handler.setLevel(logging.DEBUG)
+
     formatter = logging.Formatter('%(asctime)s - [%(levelname)s]: %(message)s')
+
     handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
     logger.addHandler(handler)
+    logger.addHandler(file_handler)
     return logger
